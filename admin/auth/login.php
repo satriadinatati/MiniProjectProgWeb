6d4e4039afe9cb20c4dc9fb89ea0032d3fbb8a4f
ivanaflrn <79034387+ivanaflrn@users.noreply.github.com>
2022-05-24 15:04:29 +0700

    template login

---
+++ admin/auth/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../assets/css/admin.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="card card-30 card-mid">
+            <div class="card-title">Login</div>
+            <div class="form">
+                <div class="form-input">
+                    <label for="username">Username</label>
+                    <input class="input" id="username" type="text" placeholder="Username">
+                </div>
+
+                <div class="form-input">
+                    <label for="password">Password</label>
+                    <input class="input" id="password" type="password" placeholder="Password">
+                </div>
+
+                <div class="form-input">
+                    <input type="submit" value="Login" class="btn btn-primary" >
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+</html>
